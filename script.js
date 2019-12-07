9b3a65ea14c5f4d7608f73bb19cdabc50431ac80
kensuke goto <utogoken2@yahoo.co.jp>
2019-12-07 15:52:59 +0900

    木を作成するメソッドを１つのオブジェクトにまとめる

--- script.js
+++ script.js
@@ -1,120 +1,157 @@
-const ns = "http://www.w3.org/2000/svg";
-const xlinkNS = "http://www.w3.org/1999/xlink";
-const outerRect = document.getElementById("outerRect");
-const terrain = document.getElementById("terrain");
-const MAX_WIDTH = +outerRect.getAttributeNS(null,"width");
-const MAX_HEIGHT = +outerRect.getAttributeNS(null,"height");
-const MIN_HEIGHT = +terrain.querySelector("rect").getAttributeNS(null,"y");
-
-const pineElement = document.querySelector("#pine path");
-const pineButton = document.getElementById("pineButton");
-
-const oak = document.querySelector("#oak");
-const oakButton = document.getElementById("oakButton");
 
 const treeButtons = document.getElementById("treeButtons");
-treeButtons.addEventListener("click",treeDispacher);
 
-function treeDispacher(e){
+document.addEventListener("DOMContentLoaded",()=>{
+
+  const outerRect = document.getElementById("outerRect");
+  const terrain = document.getElementById("terrain");
+  const MAX_WIDTH = +outerRect.getAttributeNS(null,"width");
+  const MAX_HEIGHT = +outerRect.getAttributeNS(null,"height");
+  const MIN_HEIGHT = +terrain.querySelector("rect").getAttributeNS(null,"y");
+  
+  const pineElement = document.querySelector("#pine path");
+  const oakElement = document.querySelector("#oak");
+
+  treeGenerator.init({outerRect,terrain,MAX_WIDTH,MAX_HEIGHT,MIN_HEIGHT,pineElement,oakElement});
+
+});
+
+const treeGenerator = {
+
+  init(config){
+    for(prop in config){
+      this[prop] = config[prop]
+    }
+    this.myTrees = [];
+  },
+
+
+  createOaks(){
+
+    for(let i = 0; i < 20; i++){
+
+      const x = this.getRandom(this.MAX_WIDTH,1);
+      const y = this.getRandom(this.MAX_HEIGHT,this.MIN_HEIGHT) - 100;
+      const newElement = this.oakElement.cloneNode(true);
+      newElement.removeAttributeNS(null,"id");
+  
+      this.setSVGAttributes(newElement,{
+        class:"generated-oak",
+        "data-yAxis":y + 100,
+        x,y
+      });
+  
+      const rect = newElement.getElementsByTagName("rect")[0];
+      this.setSVGAttributes(rect,{x,y})
+  
+      const ellipses = newElement.getElementsByTagName("ellipse");
+  
+      for(let i = 0; i < ellipses.length; i++){
+        this.setSVGAttributes(ellipses[i],{
+          "cx":((i + 1)*10) + x - 20,
+          "cy": this.getRandom(y - 10,y + 10)
+        })
+      }
+  
+      this.myTrees.push(newElement);
+    }
+    const sortedTrees = this.orderByYAxis(this.myTrees);
+    this.appendTreesInOrder(sortedTrees);
+
+
+
+  },
+
+
+  createPines(){
+
+    for(let i = 0;i < 20; i++){
+      const pineClone = this.pineElement.cloneNode();
+      const x = this.getRandom(this.MAX_WIDTH,1);
+      const y = this.getRandom(this.MAX_HEIGHT,this.MIN_HEIGHT)
+      const randomWidth = this.getRandom(100,50)
+      const sign = Math.random() * 2 > 1 ? 1 : -1;
+        
+      this.setSVGAttributes(pineClone,{
+        d:`M${x} ${y},H${x + (sign * randomWidth)},L${x + (sign * randomWidth)/2} ${y - randomWidth * 2}`,
+        class:"generated-pine",
+        "data-yAxis":y
+  
+      })
+      this.myTrees.push(pineClone);
+    }
+     const sortedTrees = this.orderByYAxis(this.myTrees);
+     this.appendTreesInOrder(sortedTrees);
+
+  },
+
+
+  removeAllTrees(){
+    for(const prop of this.myTrees){
+      this.terrain.removeChild(prop);
+    }
+    this.myTrees.length = 0;
+  },
+
+
+  appendTreesInOrder(trees){
+    for(let prop of trees){
+      this.terrain.appendChild(prop);
+    }
+  },
+
+
+  setSVGAttributes(element,obj){
+    for(let prop in obj){
+      element.setAttributeNS(null,prop,obj[prop]);
+    }
+  },
+
+
+  getRandom(max,min){
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  },
+
+
+  orderByYAxis(trees) {
+    const sortedTrees = trees.sort((a,b)=>{
+      const ay = a.dataset.yAxis;
+      const by = b.dataset.yAxis;
+      if(ay > by) return 1;
+      else if (ay < by) return -1;
+      else return 0;
+    })
+    return sortedTrees;
+   
+  }
+  
+}
+
+treeButtons.addEventListener("click",(e)=>{
   switch(e.target.id){
     case 'pineButton':
-      createPines();
-      break;
-    case 'removeButton':
-      removeAllTrees();
+      treeGenerator.createPines();
       break;
     case 'oakButton':
-      createOaks();
+      treeGenerator.createOaks();
       break;
+    case 'removeButton':
+      treeGenerator.removeAllTrees();
+      break;
+
     default:
       break;
   }
-}
-
-let myTrees = [];
-function setSVGAttributes(element,obj){
-  for(let prop in obj){
-    element.setAttributeNS(null,prop,obj[prop]);
-  }
-}
-
-
-function createOaks() {
-  for(let i = 0; i < 20; i++){
-
-    const x = getRandom(MAX_WIDTH,1);
-    const y = getRandom(MAX_HEIGHT,MIN_HEIGHT) - 100;
-    const newElement = oak.cloneNode(true);
-    newElement.removeAttributeNS(null,"id");
-
-    setSVGAttributes(newElement,{
-      class:"generated-oak",
-      "data-yAxis":y + 100,
-      x,y
-    });
-
-    const rect = newElement.getElementsByTagName("rect")[0];
-    setSVGAttributes(rect,{x,y})
-
-    const ellipses = newElement.getElementsByTagName("ellipse");
-
-    for(let i = 0; i < ellipses.length; i++){
-      setSVGAttributes(ellipses[i],{
-        "cx":((i + 1)*10) + x - 20,
-        "cy": getRandom(y - 10,y + 10)
-      })
-    }
-
-    myTrees.push(newElement);
-  }
-  orderByYAxis();
-}
-
-function orderByYAxis() {
-  const sortedTrees = myTrees.sort((a,b)=>{
-    const ay = a.dataset.yAxis;
-    const by = b.dataset.yAxis;
-    if(ay > by) return 1;
-    else if (ay < by) return -1;
-    else return 0;
-  })
-  for(let prop of sortedTrees){
-    terrain.appendChild(prop);
-  }
-  myTrees = [];
-}
+});
 
 
 
-function createPines() {
-  for(let i = 0;i < 20; i++){
-    const pineClone = pineElement.cloneNode();
-    const x = getRandom(MAX_WIDTH,1);
-    const y = getRandom(MAX_HEIGHT,MIN_HEIGHT)
-    const randomWidth = getRandom(100,50)
-    const sign = Math.random() * 2 > 1 ? 1 : -1;
-      
-    setSVGAttributes(pineClone,{
-      d:`M${x} ${y},H${x + (sign * randomWidth)},L${x + (sign * randomWidth)/2} ${y - randomWidth * 2}`,
-      class:"generated-pine",
-      "data-yAxis":y
 
-    })
-    myTrees.push(pineClone);
-  }
-  orderByYAxis();
-}
 
-function getRandom(max,min){
-  return Math.floor(Math.random() * (max - min + 1)) + min;
-}
 
-function removeAllTrees() {
-  for(const prop of myTrees){
-    terrain.removeChild(prop);
-  }
 
-}
+
+
 
 document.getElementById("save").addEventListener("click",function(e){
   const {format} = e.target.dataset;
@@ -122,22 +159,22 @@ document.getElementById("save").addEventListener("click",function(e){
     const name = document.getElementById("fileName").value;
     const svgData = document.getElementById("svg").outerHTML;
 
-    imageSaver.init(name,format,svgData);
+    imageSaver.save(name,format,svgData);
   }
   
 });
 
 const imageSaver = {
 
-  init(name,format,svgData){
+  save(name,format,svgData){
 
     this.name = name;
     this.format = format;
     this.svgData = svgData;
-    this.saveImage();
+    this.parseImage();
   },
 
-  saveImage(){
+  parseImage(){
 
     if(this.name.length < 1) this.name = "unnamed";
 
@@ -186,17 +223,3 @@ const imageSaver = {
 }
 
 
-
-function downloadImg(name,type,href){
-
-
-
-}
-
-
-function convertToPNG(svgUrl){
-
-
-  
-
-}
